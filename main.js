f5374ff54ce5d50983f2fe9b268e1aabc8eb4d50
gugu5 <augustin.maillefer@gmail.com>
2021-05-01 16:12:58 +0200

    ss

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 
+import * as d3 from "d3";
+//const d3 = require('d3');
+
 //Importation des données
-data = d3
+const data = d3
     .csv(
         'FichierCompletBaseANONYMEcleaned.xlsx', d3.autotype
     )
     //le + sert à changer le type en int. Possible de faire +data ?
     //d3.autotype devrait s'en sortir
 
-
+//afficher console
 console.log(data)
 
 
